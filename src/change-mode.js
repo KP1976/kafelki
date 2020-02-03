c30cdc6e02753e979ed02b84e57b2398506f43ec
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2020-02-04 00:06:16 +0100

    Change layout: first dark mode

--- src/change-mode.js
+++ src/change-mode.js
@@ -1,15 +1,32 @@
 const body = document.querySelector('body');
-const startButton = document.querySelector('.radios-container__button');
+const startButton = document.querySelector('.button');
+const backgroundColor = document.styleSheets[1].cssRules[24].style;
+const radioLabels = document.querySelectorAll('.radios-container__radio-label');
 const switcher = document.querySelector('.switch-mode-container__switch');
+const titles = document.querySelectorAll('.title');
 
 const switchColorTheme = () => {
 	switcher.addEventListener('click', function() {
-		if (!this.checked) {
-			body.classList.add('dark-mode');
-			startButton.classList.add('dark-mode');
+		if (this.checked) {
+			body.classList.add('light-mode');
+			startButton.classList.add('light-mode');
+			for (let radioLabel of radioLabels) {
+				radioLabel.classList.add('light-mode');
+			}
+			for (let title of titles) {
+				title.classList.add('light-mode');
+			}
+			backgroundColor.setProperty('background-color', '#ececec');
 		} else {
-			body.classList.remove('dark-mode');
-			startButton.classList.remove('dark-mode');
+			body.classList.remove('light-mode');
+			startButton.classList.remove('light-mode');
+			for (let radioLabel of radioLabels) {
+				radioLabel.classList.remove('light-mode');
+			}
+			for (let title of titles) {
+				title.classList.remove('light-mode');
+			}
+			backgroundColor.setProperty('background-color', '#696969');
 		}
 	});
 };
