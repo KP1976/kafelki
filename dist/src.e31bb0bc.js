cdf6fc400a2d9f85e06ed2ac2ac7750ec9aebce8
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2020-02-29 18:59:05 +0100

    Add styles for min 1200px layout (4x3 grid light mode)

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -330,7 +330,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65468" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55367" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
